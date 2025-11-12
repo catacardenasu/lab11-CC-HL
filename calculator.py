ac44202379a8d66b2c3ff1d1a1ab77bc8a6ac38b
Catalina Cardenas <catacardenasu@gmail.com>
2025-11-12 18:55:18 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -12,9 +12,11 @@ def sub(a, b):
 def mul(a, b):
     return a * b
 def div(a, b):
-    return a / b
+    return b / a
 def log(a, b):
-    return math.log(a, b)
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("value error")
+    return math.log(b, a)
 def exp(a, b):
     return a**b
 
