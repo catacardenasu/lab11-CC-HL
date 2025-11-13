f34c6fef62d69d92d77dd07ab96fdeec00df652b
Catalina Cardenas <catacardenasu@gmail.com>
2025-11-12 19:34:13 -0500

    done

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(-2, 3), -6)
+        self.assertAlmostEqual(mul(2.2, 3), 6.6)
+        self.assertAlmostEqual(mul(0, 3), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(1, 2), 2)
+        self.assertAlmostEqual(div(-2, 4), -2)
+        self.assertAlmostEqual(div(2, 5), 2.5)
+
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,19 +38,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+    ## call log function inside, example:
+        with self.assertRaises(ValueError):
+            log(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(0, 5), 5)
+        self.assertAlmostEqual(hypotenuse(1.5, 2.0), math.sqrt(1.5 ** 2 + 2.0 ** 2))
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-4)
+        self.assertEqual(square_root(9), 3)
+        self.assertEqual(square_root(0), 0)
     #     # Test basic function
     #     fill in code
     ##########################
