a272ba5406d9ccf4dcf989ec754b99183671f9d3
Catalina Cardenas <catacardenasu@gmail.com>
2025-11-12 19:14:44 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -20,31 +20,17 @@ def div(a, b):
     return b / a # raise ZeroDivisionError if a == 0
 
 def log(a, b):
-    return math.log(b,a)# use math library + raise ValueError
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Input must be a positive number.")
-
-
+    return math.log(b,a)
 def exp(a, b):
-    return math.power(a, b)
-import math
+    return a**b
+
 def square_root(a):
     return math.sqrt(a)# raise ValueError if a < 0
 def hypotenuse(a, b):
     return math.hypot(a, b) # can have negative nums
-def add(a, b):
-    return a + b
-def sub(a, b):
-    return a - b
-def mul(a, b):
-    return a * b
 
-def log(a, b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("value error")
-    return math.log(b, a)
-def exp(a, b):
-    return a**b
 
 
 
